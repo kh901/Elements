c8793b381b14b4417baa7f461f810b7082b061d7
jy761 <jy761@uowmail.edu.au>
2014-10-09 00:44:59 +1100

    made menu class header

--- menu.cpp
+++ menu.cpp
@@ -1,55 +1,4 @@
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-#include <string>
-
-//INPUT STUFF
-#include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
-//END INPUT STUFF
-
-class Menu
-{
-	public:
-		Menu(std::ostringstream *, std::ostringstream *, int *);
-		~Menu();
-		
-		void setOptions(const std::string, std::string[], const int, const int);
-		void setDescriptions(std::string *);
-		void setValues(int *);
-		int doMenu();
-	private:
-		std::ostringstream * buffer;
-		std::ostringstream * lastBuffer;
-		int * bufferSize;
-	
-		std::string title;
-		std::string * options;
-		int optionNum;
-		int displayRow;
-		std::string * descriptions;
-		int * values;
-
-		int getCmd();
-};
-
-class MenuManager 
-{
-	public:
-		MenuManager(const int);
-		~MenuManager();
-
-		
-	private:
-		int displayRow;
-		std::ostringstream buffer;
-		std::ostringstream lastBuffer;
-		int bufferSize;
-};
+#include "menu.h"
 
 Menu::Menu(std::ostringstream *buf, std::ostringstream *prevBuf, int *bufSize)
 {
@@ -210,14 +159,14 @@ int Menu::doMenu()
 				case RIGHTKEY:	//Increase value
 					if (values)
 					{
-						if (values[option] < 10)
+						if (values[option] < 10)		// Todo: change to actual max value 
 						{ values[option]++; }
 					}
 				break;
 
 				case BACKSPACE:     //Back/Cancel
                 case DELETE:
-
+					
 				break;
 			}
 		}
---
+++ menu.h
@@ -0,0 +1,57 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+//INPUT STUFF
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+//END INPUT STUFF
+
+class Menu
+{
+	public:
+		Menu(std::ostringstream *, std::ostringstream *, int *);
+		~Menu();
+		
+		void setOptions(const std::string, std::string[], const int, const int);
+		void setDescriptions(std::string *);
+		void setValues(int *);
+		int doMenu();
+	private:
+		std::ostringstream * buffer;
+		std::ostringstream * lastBuffer;
+		int * bufferSize;
+	
+		std::string title;
+		std::string * options;
+		int optionNum;
+		int displayRow;
+		std::string * descriptions;
+		int * values;
+
+		int getCmd();
+};
+
+class MenuManager 
+{
+	public:
+		MenuManager(const int);
+		~MenuManager();
+
+		
+	private:
+		int displayRow;
+		std::ostringstream buffer;
+		std::ostringstream lastBuffer;
+		int bufferSize;
+};
+
+#endif
