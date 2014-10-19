481746606ecb713d2d38f373c37a10a262d9771b
ad830 <ad830@uowmail.edu.au>
2014-10-19 22:29:42 +1100

    Edited networking files

--- Network/client.cpp
+++ Network/client.cpp
@@ -7,19 +7,25 @@ using namespace sf;
 
 int main(){
 
+	int option[1];
 	TcpSocket socket;
 	Socket::Status status = socket.connect("localhost",60000);
-	if(status	== Socket::Done){
-		cout<< "status" <<endl;
+	if(status == Socket::Done){
+		//cout<< "status" <<endl;
 		char data[100] = "";
 		size_t rec;
 		socket.receive(data,100,rec);
 		cout<<data<<endl;
+		cin >> option[0];
+		if (option[0] > 5 || option[0] < 1)
+		{
+			cout << "ERROR" << endl;
+			return -1;
+		}
+		socket.send(option, 1);
 	}
-
-
-
-
+	
+	return 0;
 }
 
 
