b850494fcb415e0cd1b66f7ed1a89a075f5cb6e2
jy761 <jy761@uowmail.edu.au>
2014-10-22 01:11:42 +1100

    added write/read map and string for binary files

--- File.cpp
+++ File.cpp
@@ -6,8 +6,18 @@ void appendData(std::ofstream &ofs, const A &data)
 	ofs.write((char*)&data, sizeof(A));
 }
 
+void appendString(std::ofstream &ofs, const std::string &strdata)
+{
+	// write size of string
+	int size = strdata.length();
+	ofs.write((char*)&size, sizeof(int));
+	
+	// write string data
+	ofs.write(strdata.c_str(), size * sizeof(char));
+}
+
 template <class B>
-void appendVector(std::ofstream &ofs, const vector<B> &vecdata)
+void appendVector(std::ofstream &ofs, const std::vector<B> &vecdata)
 {
 	// write size of vector
 	int size = vecdata.size();
@@ -17,11 +27,29 @@ void appendVector(std::ofstream &ofs, const vector<B> &vecdata)
 	ofs.write((char*)&vecdata[0], vecdata.size() * sizeof(B));
 }
 
+template<class H, class J>
+void appendMap(std::ofstream &ofs, const std::map<H, J> &mapdata)
+{
+	// write size of map
+	int size = mapdata.size();
+	ofs.write((char*)&size, sizeof(int));
+	
+	// write map data
+	typename std::map<H, J>::iterator it;
+	for (it = mapdata.begin(); it != mapdata.end(); ++it)
+	{
+		// write the key
+		ofs.write((char*)&(it->first), sizeof(H));
+		// write the value
+		ofs.write((char*)&(it->second), sizeof(J));
+	}
+}
+
 template<class T>
-void writeFile(const std::string &filename, const vector<T> &myvec)
+void writeFile(const std::string &filename, const std::vector<T> &myvec)
 {
 	std::ofstream fout;
-	fout.open(filename.c_str(), ios::binary);
+	fout.open(filename.c_str(), std::ios::binary);
 	
 	appendVector<T>(fout, myvec);
 	
@@ -35,7 +63,7 @@ void readData(std::ifstream &ifs, W &data)
 }
 
 template<class Z>
-bool readVector(std::ifstream &ifs, vector<Z> &vecdata)
+bool readVector(std::ifstream &ifs, std::vector<Z> &vecdata)
 {
 	// read in size of vector from file
 	int size;
@@ -58,8 +86,55 @@ bool readVector(std::ifstream &ifs, vector<Z> &vecdata)
 	return true;
 }
 
+template<class K, class L>
+void readMap(std::ifstream &ifs, std::map<K, L> &mapdata)
+{
+	// read in size of map from file
+	int size;
+	ifs.read((char*)&size, sizeof(int));
+	
+	if (size <= 0)
+	{
+		return false;	
+	}
+	mapdata.clear();
+	mapdata.reserve(size);
+	
+	// read in map data from file
+	K tmpKey; 
+	L tmpValue;
+	for (int i = 0; i < size; ++i)
+	{
+		// read key
+		ifs.read((char*)&tmpKey, sizeof(K));
+		// read value
+		ifs.read((char*)&tmpValue, sizeof(L));
+		mapdata.insert(std::pair<K, L>(tmpKey, tmpValue));
+	}
+	return true;
+}
+
+bool readString(std::ifstream &ifs, std::string &strdata)
+{
+	// read in size of string from file
+	int size;
+	ifs.read((char*)&size, sizeof(int));
+	
+	if (size <= 0)
+	{
+		return false;
+	}
+	strdata.clear();
+	
+	// read in string data from file
+	std::string tmp;
+	ifs.read((char*)&tmp, size * sizeof(char));
+	strdata = tmp;
+	return true;
+}
+
 template<class C>
-bool readFile(const std::string &filename, vector<C> &myvec) 
+bool readFile(const std::string &filename, std::vector<C> &myvec) 
 {
 	std::ifstream fin;
     fin.open(filename.c_str(), std::ios::binary);
@@ -70,6 +145,54 @@ bool readFile(const std::string &filename, vector<C> &myvec)
 	return readStatus;
 }
 
+template<class Y>
+bool readFileClass(const std::string &filename, std::vector<Y> &myvec) 
+{
+	std::ifstream fin;
+    fin.open(filename.c_str(), std::ios::binary);
+    
+    // read size of vector
+    int size;
+    fin.read((char*)&size, sizeof(int));
+    
+    if (size <= 0)
+    {
+    	fin.close();
+    	return false;
+    }
+    myvec.clear();
+    myvec.reserve(size);
+    
+    // read contents of binary file into vector
+    for (int i = 0; i < size; ++i)
+    {
+    	Y tmp;
+    	tmp.readFile(fin);		// use polymorphism to read in each element
+    	myvec.push_back(tmp);
+    }
+	fin.close();
+	return true;
+}
+
+template<class T>
+void writeFileClass(const std::string &filename, const std::vector<T> &myvec)
+{
+	std::ofstream fout;
+	fout.open(filename.c_str(), std::ios::binary);
+	
+	// write size of vector
+	int size = myvec.size();
+	fout.write((char*)&size, sizeof(int));
+	
+	// write vector data
+	for (int i = 0; i < size; ++i)
+	{
+		myvec[i].writeFile(fout); 	// use polymorphism to write each element
+	}
+	fout.close();
+}
+
+/*
 void writeSubmissionsFile(const std::string &filename, const vector<Submission> &subs)
 {
 	std::ofstream fout;
@@ -128,3 +251,4 @@ bool readSubmissionsFile(const std::string &filename, vector<Submission> &subs)
     fin.close();
     return true;
 }
+*/
--- File.h
+++ File.h
@@ -2,29 +2,72 @@
 #define FILE_H
 
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <vector>
+#include <map>
+
+class FileIO
+{
+	public:
+		virtual void writeFile(std::ofstream &) = 0;
+		virtual bool readFile(std::ifstream &) = 0;
+};
 
 template<class A>
 void appendData(std::ofstream &ofs, const A &data);
 
+void appendString(std::ofstream &ofs, const std::string &strdata);
+
+template<class H, class J>
+void appendMap(std::ofstream &ofs, const std::map<H, J> &mapdata);
+
 template <class B>
-void appendVector(std::ofstream &ofs, const vector<B> &vecdata);
+void appendVector(std::ofstream &ofs, const std::vector<B> &vecdata);
 
 template<class T>
-void writeFile(const std::string &filename, const vector<T> &myvec);
+void writeFile(const std::string &filename, const std::vector<T> &myvec);
+
+/*
+	Each class that uses this function should inherit  
+	and implement the interface described in the FileIO class.
+	
+	MyClass::writeFile(myFileStream) should use a combination of:
+		appendData<datatype>(myFileStream, this->mydata);
+		(and)
+		appendVector<datatype>(myFileStream, this->mylist);
+	
+	in order to save their class's data to a binary file properly.
+*/
+template<class T>
+void writeFileClass(const std::string &filename, const std::vector<T> &myvec);
 
 template<class W>
 void readData(std::ifstream &ifs, W &data);
 
+bool readString(std::ifstream &ifs, std::string &strdata);
+
 template<class Z>
-bool readVector(std::ifstream &ifs, vector<Z> &vecdata);
+bool readVector(std::ifstream &ifs, std::vector<Z> &vecdata);
+
+template<class K, class L>
+void readMap(std::ifstream &ifs, std::map<K, L> &mapdata);
 
 template<class C>
-bool readFile(const std::string &filename, vector<C> &myvec);
+bool readFile(const std::string &filename, std::vector<C> &myvec);
 
-// Submissions
-void writeSubmissionsFile(const std::string &filename, const vector<Submission> &subs);
-bool readSubmissionsFile(const std::string &filename, vector<Submission> &subs)
+/*
+	Each class that uses this function should inherit  
+	and implement the interface described in the FileIO class.
+	
+	MyClass::readFile(myFileStream) should use a combination of:
+		readData<datatype>(myFileStream, this->mydata);
+		(and)
+		readVector<datatype>(myFileStream, this->mylist);
+	
+	in order to read in their class's data from a vector saved in a binary file properly.
+*/
+template<class Y>
+bool readFileClass(const std::string &filename, std::vector<Y> &myvec);
 
 #endif
