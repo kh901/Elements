5fef5da93dc66922e6487363ab3cd62ec40c605c
kieran <kieran.haavisto@gmail.com>
2014-10-21 17:06:49 +1100

    modified stuff

--- Network/client.cpp
+++ Network/client.cpp
@@ -1,39 +1,46 @@
 #include<iostream>
 #include<SFML/Network.hpp>
 #include<cstring>
+#include<SFML/System/Time.hpp>
+
 using namespace std;
 using namespace sf;	
 
 int main(){
 
 	Packet login;
+
 	string username;
 	string password;
 	int level = 0;
 
 	TcpSocket socket;
-	Socket::Status status = socket.connect("localhost",60000);
+	
+	Time time = seconds(7);
+	
+	Socket::Status status = socket.connect("localhost",60000,time);
+	
 	if(status == Socket::Done){
-		char data[100] = "";
+		/*char data[100] = "";
 		size_t rec;
-		socket.receive(data,100,rec);
-		cout << data;
-		cin >> username;
-		cout << endl;
-		strcpy(data,"");
-		socket.send(data,100);
-		socket.receive(data,100,rec);
-		cout << data;
-		cin >> password;
-		cout << endl;
-		strcpy(data,"");
-		socket.send(data,100);
-		socket.receive(data,100,rec);
-		cout << data;
-		cin >> level;
-		cout << endl;
-		login << username << password << level;
-		socket.send(login);
+		socket.receive(data,100,rec);*/
+		bool valid = false;
+		Packet response;
+		while(valid==false){
+		
+			cout<<"Username: ";
+			cin>>username;
+			cout<<endl;
+		
+			cout<<"Password: ";
+			cin>>password;
+			cout<<endl;
+		
+			login << username << password;
+			socket.send(login);
+			socket.receive(response);
+			response >> valid;
+		}
 	}
 	
 	return 0;
