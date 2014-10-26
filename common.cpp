d743d924f972ae99d854eab5ea334c2698aa45d4
jy761 <jy761@uowmail.edu.au>
2014-10-27 02:26:50 +1100

    more timing functions

--- common.cpp
+++ common.cpp
@@ -34,3 +34,67 @@ std::string getDate()
 	return os.str();
 }
 
+std::string timeToString(time_t val)
+{
+	std::ostringstream os;
+	struct tm * currentTime = localtime(&val);
+	int seconds = currentTime->tm_sec;
+	int minutes = currentTime->tm_min;
+	int hours = currentTime->tm_hour;
+	os << std::setfill('0') << std::setw(2) << hours << ':';
+	os << std::setfill('0') << std::setw(2) << minutes << ':';
+	os << std::setfill('0') << std::setw(2) << seconds;
+	os << ' ';
+	int day = currentTime->tm_mday;
+	int month = 1 + currentTime->tm_mon; 
+	int year = 1900 + currentTime->tm_year; 
+	os << std::setfill('0') << std::setw(2) << day << '-';
+	os << std::setfill('0') << std::setw(2) << month << '-';
+	os << std::setfill('0') << std::setw(2) << year;
+	return os.str();
+}
+
+time_t setWeekDeadlineFromNow()
+{
+	// get current time
+	time_t val = time(NULL);
+	// advance it a week
+	val = getWeekAhead(val);
+	// alter the time to be 12 midday
+	struct tm * curTime = localtime(&val);
+	curTime->tm_hour = 12;
+	curTime->tm_min = 0;
+	curTime->tm_sec = 0;
+	return mktime(curTime);
+}
+
+time_t getTimeValue()
+{
+	return time(NULL);
+}
+
+time_t getDayAhead(time_t cur)
+{
+	// add a day
+	static const long int DAY_TIME = 86400;
+	cur += DAY_TIME;
+	// set the time to be 12 midday
+	struct tm * curTime = localtime(&cur);
+	curTime->tm_hour = 12;
+	curTime->tm_min = 0;
+	curTime->tm_sec = 0;
+	return mktime(curTime);
+}
+time_t getWeekAhead(time_t cur)
+{
+	// add a week
+	static const long int WEEK_TIME = 604800;
+	cur += WEEK_TIME;
+	// set the time to be 12 midday
+	struct tm * curTime = localtime(&cur);
+	curTime->tm_hour = 12;
+	curTime->tm_min = 0;
+	curTime->tm_sec = 0;
+	return mktime(curTime);
+}
+
--- common.h
+++ common.h
@@ -11,5 +11,15 @@ int Ceil(const int &, const int &);
 std::string getTimestamp();
 // gets current date in DD-MM-YYYY format
 std::string getDate();
+// gets current time as a time_t (long int)
+time_t getTimeValue();
+// gets a day later at 12 midday from given value
+time_t getDayAhead(time_t);
+// gets a week later at 12 midday from given value 
+time_t getWeekAhead(time_t);
+// converts time_t to show TIME DATE as format (HH:MM:SS DD-MM-YYYY)
+std::string timeToString(time_t);
+// gets a time value (long int) that is a week later from now at 12 midday
+time_t setWeekDeadlineFromNow();
 
 #endif
