9859513e0e802b7db50c7693accc67d410f7eab0
jy761 <jy761@uowmail.edu.au>
2014-10-29 10:34:55 +1100

    removed network folder because unnecessary

--- Network/buildclient.sh
+++
@@ -1,5 +0,0 @@
-g++ client.cpp -c
-g++ ../Account.cpp -c
-g++ ../File.cpp -c
-g++ ../Submission.cpp -c
-g++ -o clienttest client.o Submission.o Account.o File.o -lsfml-network -lsfml-system
--- Network/buildserver.sh
+++
@@ -1,5 +0,0 @@
-g++ server.cpp -c
-g++ ../Account.cpp -c
-g++ ../File.cpp -c
-g++ ../Submission.cpp -c
-g++ -o servertest server.o Account.o Submission.o File.o -lsfml-network -lsfml-system
